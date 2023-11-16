21e90e0fcdfd37fa79fc57a9cdb16740126ce19f
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-16 11:24:33 +0200

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,46 @@
-// Add imports above this line
+// Add imports above this lineimport
 import { galleryItems } from './gallery-items';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-console.log('Hello');
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML('beforeend', getClick(galleryItems));
+galleryList.addEventListener('click', getImages);
+
+function getClick(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) => `
+    
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+     /></a>
+</li>
+ `
+    )
+    .join('');
+}
+console.log('Nata');
+function getImages(event) {
+  event.preventDefault();
+
+  if (event.target === event.currentTarget) {
+    return;
+  } else {
+    const instance = SimpleLightbox.create(
+      `
+        <img src="${event.target.dataset.source}" width="250px" height="250px">
+    `
+    );
+    instance.show();
+  }
+}
+
 console.log(galleryItems);
