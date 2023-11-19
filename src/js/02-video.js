97e2601109048653d58adf01046e155e1e591ee5
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-19 18:54:46 +0200

    getTimePlay change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,44 @@
 import player from '@vimeo/player';
-console.log(player);
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const Player = new player(iframe);
 
-player.on('play', function () {
+Player.on('play', getTimePlay);
+
+function getTimePlay(data) {
+  {
+    duration: 61.857;
+    percent: 0.049;
+    seconds: 3.034;
+  }
+  console.log(localStorage);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  localStorage.setCurrentTime(30.456);
   console.log('played the video!');
-});
+}
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+console.log(Player);
+
+// Player.setCurrentTime()
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
+
+const throttle = require('lodash.throttle');
+console.log(throttle);
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
