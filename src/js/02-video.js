4dd98a3e240af4fa39093c1fbf584b0d22be82e0
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-21 17:32:57 +0200

    change files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,33 +12,9 @@ function getTimePlay(data) {
     percent: 0.049;
     seconds: 3.034;
   }
-  console.log(localStorage);
   localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-  localStorage.setCurrentTime(30.456);
+  Player.setCurrentTime(data.seconds);
   console.log('played the video!');
 }
 
-console.log(Player);
-
-// Player.setCurrentTime()
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
-
-const throttle = require('lodash.throttle');
-console.log(throttle);
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+throttle = require(getTimePlay, 1000);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const getForm = document.querySelector('.feedback-form');
+// console.log(getForm.email);
+// console.log(getForm.message);
+const getInput = document.querySelector('input');
+getInput.addEventListener(
+  'click',
+  getEmail
+  //   _.throttle(() => {}),
+  //   500
+);
+
+function getEmail(event) {
+  const userInfo = {
+    email: getForm.email.value,
+    massege: getForm.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+
+  //   form.reset();
+}
+
+// console.log(getInput);
